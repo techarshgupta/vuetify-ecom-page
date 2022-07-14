6030e5dbaf27fa045be288870c59d01bd58e5af1
Harsh Gupta <techarshgupta@gmail.com>
2022-07-14 10:25:01 +0530

    vuetify ecom page

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <Navbar class="index" />
+    <Dashboard />
+    <Footer />
+  </v-app>
+</template>
+
+<script>
+  import Navbar from '../components/Navbar'
+  import Dashboard from '../components/Dashboard'
+  import Footer from '../components/Footer'
+
+  export default {
+    name: 'Home',
+
+    components: {
+      Navbar,
+      Dashboard,
+      Footer
+
+    },
+  }
+</script>
+<style scoped>
+.index{
+  z-index: 9999;
+}
+</style>
